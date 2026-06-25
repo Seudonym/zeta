ffd46130d24d26bab600c7a5916c1958b78bc6ed
Seudonym <wk170179@gmail.com>
2026-06-25 22:40:32 +0530

    feat: add chat loop and history

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::io;
+
 use color_eyre::eyre::Result;
-use rig::{client::CompletionClient, completion::Prompt, providers::ollama::Client};
+use rig::{
+    OneOrMany,
+    client::CompletionClient,
+    completion::{Chat, Message},
+    message::{AssistantContent, UserContent},
+    providers::ollama::Client,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -10,8 +18,32 @@ async fn main() -> Result<()> {
         .agent("smollm")
         .preamble("You are a local assistant. Say whatever.")
         .build();
+    let mut chat_history = Vec::<Message>::new();
 
-    let response = agent.prompt("What model are you?").await?;
-    println!("{}", response);
-    Ok(())
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let input = input.trim();
+
+        if input.is_empty() {
+            continue;
+        }
+
+        let mut chat_history_clone = chat_history.clone();
+        let response = agent.chat(input, &mut chat_history_clone);
+
+        let user_message = Message::User {
+            content: OneOrMany::one(UserContent::text(input)),
+        };
+        chat_history.push(user_message);
+
+        let response = response.await?;
+        println!("{}", response);
+
+        let assistant_message = Message::Assistant {
+            id: None,
+            content: OneOrMany::one(AssistantContent::from(response)),
+        };
+        chat_history.push(assistant_message);
+    }
 }
