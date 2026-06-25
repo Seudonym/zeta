885abd9e613eab8f0c6ff993d917e6d0a7310150
Seudonym <wk170179@gmail.com>
2026-06-25 22:23:43 +0530

    feat: rig first api call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("lmao");
+use color_eyre::eyre::Result;
+use rig::{client::CompletionClient, completion::Prompt, providers::ollama::Client};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let client = Client::new("http://127.0.0.1:11434")?;
+    let agent = client
+        .agent("smollm")
+        .preamble("You are a local assistant. Say whatever.")
+        .build();
+
+    let response = agent.prompt("What model are you?").await?;
+    println!("{}", response);
+    Ok(())
 }
