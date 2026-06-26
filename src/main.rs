66e7d01adbe4333166ee0bc4729aeae5db3411e1
Seudonym <wk170179@gmail.com>
2026-06-26 09:27:59 +0530

    fix: duplicates in chat history

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use std::io;
 
 use color_eyre::eyre::Result;
 use rig::{
-    OneOrMany,
     client::CompletionClient,
     completion::{Chat, Message},
-    message::{AssistantContent, UserContent},
     providers::ollama::Client,
 };
 
@@ -29,21 +27,10 @@ async fn main() -> Result<()> {
             continue;
         }
 
-        let mut chat_history_clone = chat_history.clone();
-        let response = agent.chat(input, &mut chat_history_clone);
+        println!("{:?}", chat_history);
 
-        let user_message = Message::User {
-            content: OneOrMany::one(UserContent::text(input)),
-        };
-        chat_history.push(user_message);
-
+        let response = agent.chat(input, &mut chat_history);
         let response = response.await?;
         println!("{}", response);
-
-        let assistant_message = Message::Assistant {
-            id: None,
-            content: OneOrMany::one(AssistantContent::from(response)),
-        };
-        chat_history.push(assistant_message);
     }
 }
